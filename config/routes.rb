5a8f87e3a9ec4f9f275032741b0e6e880a87d150
Tim <timothee.gauguet@orange.fr>
2016-03-13 20:15:45 +0100

    modif fichier routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root "welcome#index"
-  resources :newsletters, :only => [:create]
+	constraints subdomain: false do
+	  get ':any', to: redirect(subdomain: 'www', path: '/%{any}'), any: /.*/
+	end
+  	root "welcome#index"
+  	resources :newsletters, :only => [:create]
 end
