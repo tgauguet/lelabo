83dbe95d4693824e17a50344f5db2e96fb2f6279
Tim <timothee.gauguet@orange.fr>
2016-03-13 19:46:10 +0100

    modif config

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Lelabo
   class Application < Rails::Application
+    config.i18n.default_locale = :fr
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -19,8 +20,28 @@ module Lelabo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.generators do |g|
+        g.assets = false
+        g.helper = false
+    end
+    config.filter_parameters += [:password]
+    config.log_tags = [
+    :host,
+    :remote_ip,
+    lambda { |request| "#{request.uuid}"[0..15] },
+    lambda { |request| "#{request.cookie_jar["_on_ruby_session"]}"[0..15] },
+    ]
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.encoding = "utf-8"
+    config.autoload_paths += Dir["#{Rails.root}/app/services"]
+    config.autoload_paths += Dir["#{Rails.root}/app/validators"]
+    config.autoload_paths += Dir["#{Rails.root}/app/jobs"]
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.enabled = true
+    config.eager_load_paths += %W(
+    #{config.root}/app/services
+    )
   end
 end
