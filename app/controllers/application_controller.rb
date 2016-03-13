54c429257ba2a706176c963f26b4e5edfc776d3a
Tim <timothee.gauguet@orange.fr>
2016-03-13 20:22:30 +0100

    modif app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :set_newsletter
+  before_filter :add_www_subdomain
 
   def set_newsletter
     @newsletter = Newsletter.new
+  end
+
+  private
+  def add_www_subdomain
+    unless /^www/.match(request.host)
+      redirect_to("#{request.protocol}www.#{request.host_with_port}",status: 301)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-	constraints subdomain: false do
-	  get ':any', to: redirect(subdomain: 'www', path: '/%{any}'), any: /.*/
-	end
   	root "welcome#index"
   	resources :newsletters, :only => [:create]
 end
