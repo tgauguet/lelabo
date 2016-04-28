492fd3364aee388863b4918f3c24a94aef0fe6ca
Tim <timothee.gauguet@orange.fr>
2016-04-28 14:33:22 +0200

    amélioration mailing gibbon

--- app/jobs/subscribe_user_to_mailing_list_job.rb
+++ app/jobs/subscribe_user_to_mailing_list_job.rb
@@ -2,8 +2,12 @@ class SubscribeUserToMailingListJob < ActiveJob::Base
   queue_as :default
 
   def perform(user)
-    gb = Gibbon::Request.new
-    gb.lists(Rails.application.secrets.mailchimp_list_id).members.create(body: {email_address: user.email, status: "subscribed", merge_fields: {F_NAME: user.firstname, L_NAME: user.name}})
+    begin
+    	gb = Gibbon::Request.new
+    	gb.lists(Rails.application.secrets.mailchimp_list_id).members.create(body: {email_address: user.email, status: "subscribed", merge_fields: {F_NAME: user.firstname}})
+    rescue Gibbon::MailChimpError => e
+    	logger.error "Mailchimp threw an error. The code is wrong with message: #{e.message}"
+    end
   end
 end
 
