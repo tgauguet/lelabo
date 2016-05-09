be4c4b71ac80a80ee293e60a36f65de7e197cd61
Tim <timothee.gauguet@orange.fr>
2016-05-09 16:54:26 +0200

    grosse ré

--- Gemfile
+++ Gemfile
@@ -17,11 +17,7 @@ gem 'uglifier', '>= 1.3.0'
 
 gem 'coffee-rails', '~> 4.1.0'
 
-gem 'jquery-rails'
-
 gem 'aws-sdk', '< 2.0'
-
-gem 'turbolinks'
 
 gem 'jbuilder', '~> 2.0'
 
@@ -49,7 +45,10 @@ gem 'guard-rspec'
 
 gem 'paperclip'
 
-gem 'jquery-turbolinks'
+# jquery gems for rails
+gem "jquery-rails"
+
+gem 'jquery-ui-rails'
 
 gem 'puma'
 
@@ -74,5 +73,11 @@ group :development do
   gem 'web-console', '~> 2.0'
 
   gem 'spring'
+end
+
+group :assets do 
+
+  gem 'jquery-ui-rails'
+
 end
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,13 +11,15 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require jquery.turbolinks 
-//= require turbolinks
-//= require_tree .
+//= require jquery-ui
 //= require welcome
 //= require subscriptions
 //= require to_do_lists
+//= require_tree .
+
+$(function(){
+    $( "#datepicker" ).datepicker();
+});
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -45,3 +47,4 @@ $(document).ready(function(){
     $(".explaination-cntnr").show();
   });
 });
+
