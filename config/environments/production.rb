bd17f3ddc9d733b33f647ca5e40c48e6bdba45ac
Tim <timothee.gauguet@orange.fr>
2016-05-12 16:47:38 +0200

    modification solr rails env aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,13 +19,13 @@ Rails.application.configure do
     config.paperclip_defaults = {
       storage: :s3,
       s3_credentials: {
-        bucket: ENV.fetch('S3_BUCKET_NAME'),
-        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-        s3_region: ENV.fetch('AWS_REGION'),
+        bucket: ENV['S3_BUCKET_NAME'],
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        s3_region: ENV['AWS_REGION'],
       }
     }
-    
+
     # Enable Rack::Cache to put a simple HTTP cache in front of your application
     # Add `rack-cache` to your Gemfile before enabling this.
     # For large-scale production use, consider using a caching reverse proxy like
