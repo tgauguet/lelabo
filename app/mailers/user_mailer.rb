e7b696b04d59b99a01206750e764d0bfa6ca7dd2
tgauguet <tgauguet@student.42.fr>
2016-10-26 16:28:46 +0200

    added launch email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,15 @@
 class UserMailer < ApplicationMailer
-	
+
 	def welcome_email(user)
 		@user = user
 		@url = "http://www.lelabo-ondemand.com"
 		mail( to: @user.email, subject: "Bienvenue sur le Labo" )
 	end
-	
+
+	def launch_email(user)
+		@user = user
+		@url = "http://www.lelabo-ondemand.com"
+		mail( to: @user.email, subject: "Lancement officiel")
+	end
+
 end
