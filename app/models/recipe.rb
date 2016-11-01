86deabad90971bdb7417778db62a16b85907247e
tgauguet <tgauguet@student.42.fr>
2016-11-01 22:07:47 +0100

    added new model user

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -15,7 +15,7 @@ class Recipe < ActiveRecord::Base
 	has_attached_file :image, {
                                     :styles => { medium: "300x300#", small: "75x75#"},
                                     :size => { :in => 0..300.kilobytes },
-																		:default_url => "/images/missing-cake.png",
+																		:default_url => "/images/missing-cake.png"
                                 }
   validates_attachment_content_type :image,
                                     :content_type => /^image\/(png|gif|jpeg)/
