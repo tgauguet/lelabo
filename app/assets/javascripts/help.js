0b59b7844ba322a7d8415f4bb1bd3854a761d53d
tgauguet <tgauguet@student.42.fr>
2016-11-07 20:43:00 +0100

    added help index

--- app/assets/javascripts/help.js
+++ app/assets/javascripts/help.js
@@ -2,6 +2,8 @@
 $(document).ready(function(){
   $('.help-cat ul li').click(function(){
     $(".help-option").hide();
+    $(".help-cat ul li").removeClass("current-help");
+		$(this).addClass("current-help");
     $("#div"+$(this).attr('target')).show();
   });
 });
