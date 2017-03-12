47da47538606c4e8677b2cbfdb0dfb7ce1718c7b
tgauguet <tgauguet@student.42.fr>
2017-03-12 18:17:39 +0100

    added macaron image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,15 @@ Rails.application.configure do
     config.assets.js_compressor = Uglifier.new(output: {ascii_only: true, quote_keys: true})
     config.cache_classes = true
     config.action_mailer.default_url_options = {:host => "www.lelabo-ondemand.com"}
+    config.paperclip_defaults = {
+      storage: :s3,
+      s3_credentials: {
+        bucket: ENV['S3_BUCKET_NAME'],
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        s3_region: ENV['AWS_REGION'],
+      }
+    }
 
     # Eager load code on boot. This eager loads most of Rails and
     # your application in memory, allowing both threaded web servers
