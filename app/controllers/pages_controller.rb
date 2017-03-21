b74ffdb3f80edf13f78a7f308c5fb476d260ba78
tgauguet <tgauguet@student.42.fr>
2017-03-21 14:34:09 +0100

    added recipes tracking

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
 
   def metrics
-    @users = User.last(10)
-    @recipes = Recipe.last(10)
-    @assemblies = Assembly.last(10)
-    @sub = Subscription.last(10)
+    @users = User.all
   end
 
 end
